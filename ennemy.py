f83c3b1fa1a024c15f2056115f9ebad480542ed8
Jilaskel <119072453+Jilaskel@users.noreply.github.com>
2022-11-27 20:12:40 +0100

    Premiere animation !!

--- ennemy.py
+++ ennemy.py
@@ -2,11 +2,12 @@ import pygame
 from utilitaries import *
 
 class Ennemy(pygame.sprite.Sprite):
-      def __init__(self,game):
+      def __init__(self,game,x,y):
             super().__init__()
-            self.image = pygame.image.load("Assets/Ennemies/ennemy1.png").convert()    
-            self.posX = game.background.bush_width
-            self.posY = 135
+            self.images_path = "Assets/Ennemies/"
+            self.image = pygame.image.load(self.images_path+"ennemy1.png").convert()    
+            self.posX = x     # game.background.bush_width
+            self.posY = y     # 135
             self.vel = vec(0,0)
             self.vel.x = 20
 
@@ -19,6 +20,34 @@ class Ennemy(pygame.sprite.Sprite):
             window.blit(self.image, (self.posX, self.posY))  
 
 
-    
- 
+#class Gobelin(Ennemy,pygame.sprite.Sprite):
+class Gobelin(Ennemy,pygame.sprite.Sprite):
+      def __init__(self,game,x,y):
+            pygame.sprite.Sprite.__init__(self)
+            self.images_path = "Assets/Ennemies/Gobelin/WalkAnim/"
+            self.image = pygame.image.load(self.images_path+"/001.png").convert_alpha()
+            self.number_frame_walking = 20
+            self.image_walking = []
+            for i in range(1,self.number_frame_walking+1):
+                  self.image_walking.append(pygame.image.load(self.images_path+"00"+str(i)+".png").convert_alpha())   
+            self.posX = x     
+            self.posY = y     
+            self.vel = vec(0,0)
+            self.vel.x = 10
+            self.moving = False
+            self.move_frame = 0
 
+      def move(self):
+            # Will set running to False if the player has slowed down to a certain extent
+            self.posX += self.vel.x
+            self.moving = True
+
+
+      def render(self):
+            if self.moving:
+                  self.move_frame += 1 
+                  self.move_frame = self.move_frame%self.number_frame_walking
+                  window.blit(self.image_walking[self.move_frame], (self.posX, self.posY))
+            else:
+                  window.blit(self.image, (self.posX, self.posY))  
+ 
--- game.py
+++ game.py
@@ -12,15 +12,21 @@ class Game():
       def __init__(self):
             self.background = Background()
             self.tower = Tower(self)
-            self.ennemy = Ennemy(self)
+            self.all_ennemies = pygame.sprite.Group()
+            self.all_ennemies.add(Ennemy(self,self.background.bush_width,135))
+            y_milieu = self.background.menu_height + 2*self.background.square_side
+            self.all_ennemies.add(Gobelin(self,self.background.bush_width,y_milieu))
+            #self.ennemy = Ennemy(self,self.background.bush_width,135)
 
       def move_objects(self):
-            self.ennemy.move()
+            for ennemy in self.all_ennemies:
+                  ennemy.move()
 
       def render(self):
             self.background.render()
             self.tower.render()
-            self.ennemy.render()
+            for ennemy in self.all_ennemies:
+                  ennemy.render()
             pygame.display.update()      
             FPS_CLOCK.tick(FPS)
 
